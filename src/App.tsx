68126a52889e4251cb796fd550ca8f1e04ad71f6
oakhtar147 <osamaakhtar15@gmail.com>
2021-04-30 17:12:36 +0500

    skeleton components + routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route, Switch } from 'react-router-dom';
 
-function App() {
+import About from "components/About/About";
+import Skills from "components/Skills/Skills";
+import Certificates from "components/Certificates/Certificates";
+import Projects from "components/Projects/Projects";
+import Contact from "components/Contact/Contact";
+
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Switch>
+      <Route path="/" exact component={About} />
+      <Route path="/skills" exact component={Skills} />
+      <Route path="/certificates" exact component={Certificates} />
+      <Route path="/projects" exact component={Projects} />
+      <Route path="/contact" exact component={Contact} />
+    </Switch>
+    </>
   );
 }
 
---
+++ src/components/Certificates/Certificates.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Certificates = () => {
+  return <h1>Certificates page</h1>
+}
+
+export default Certificates;
