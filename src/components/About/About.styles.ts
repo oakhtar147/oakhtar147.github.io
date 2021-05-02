7ae596a550ff93f19e656b85aa29afa78364452e
oakhtar147 <osamaakhtar15@gmail.com>
2021-05-02 14:13:31 +0500

    about page

--- src/components/About/About.styles.ts
+++ src/components/About/About.styles.ts
@@ -1,1 +1,31 @@
-export {};
+import styled from "styled-components";
+import { SocialIcon } from "react-social-icons";
+
+export const RoundedImage = styled.img`
+  width: 300px;
+  height: 300px;
+  border-radius: 50%;
+  object-fit: cover;
+`;
+
+export const Description = styled.h1`
+  max-width: 500px;
+  display: "inline-block";
+  margin: 0;
+  font-size: 2.5em;
+`;
+
+export const Main = styled.main`
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  margin-top: 8%;
+`;
+
+export const StyledSocialIcon = styled(SocialIcon)`
+  margin: 1%;
+  transition: opacity 0.2s linear;
+  &:hover {
+    opacity: 0.6;
+  }
+`;
--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,9 +1,38 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
 
+import { Main, RoundedImage, Description, StyledSocialIcon } from './About.styles';
+import myImage from 'assets/me.jpg';
+
+interface Links {
+  [socialMedia: string]: string
+}
 
 const About = () => {
+  const [aboutData, setAboutData] = useState(null);
+
+  useEffect(() => {
+    ;
+  })
+
+  const SocialLinks: Links = {
+    GitHub: "https://github.com/oakhtar147",
+    LinkedIn: "https://www.linkedin.com/in/osama-akhtar-25253a1a9/",
+    Facebook: "https://www.facebook.com/OsamaXVI/",
+    Instagram: "https://www.instagram.com/_osamaakhtar/",
+    Twitter: "https://twitter.com/_osamaakhtar",
+  }
+
+
   return (
-    <h1>About Page</h1>
+    <Main>
+      <RoundedImage src={myImage} alt="My picture" />
+      <section>
+        <Description>Hi! I am Osama Akhtar, a full-stack developer!</Description>
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          {Object.keys(SocialLinks).map(site => <StyledSocialIcon url={SocialLinks[site]} fgColor="white" />)}
+        </div>
+      </section>
+    </Main>
   )
 }
 
